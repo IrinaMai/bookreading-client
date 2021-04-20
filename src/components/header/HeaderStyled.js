4a1a75a2d813020e23d95472d90046e9b7aacd7f
Viktor Gorokhov <patronusv@gmail.com>
2021-04-20 19:09:05 +0300

    completes header components

--- src/components/header/HeaderStyled.js
+++ src/components/header/HeaderStyled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const HeaderStyled = styled.div`
   background-color: var(--secondary-background);
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
   .container.header-container {
     margin: 0 auto;
     height: 60px;
@@ -16,7 +17,7 @@ const HeaderStyled = styled.div`
     font-weight: 400;
     font-size: 20px;
     line-height: 1.35;
-    color: var(--main-color);
+    color: var(--main-text);
   }
 `;
 
--- src/components/userMenu/UserMenu.js
+++ src/components/userMenu/UserMenu.js
@@ -1,29 +1,36 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 import { useWindowWidth } from '@react-hook/window-size';
 import UserName from '../userName/UserName';
 import UserMenuStyled from './UserMenuStyled';
 import mainRoutes from '../../routes/mainRoutes';
-import TrainingIcon from '../icons/TrainingIcon';
-import HomeIcon from '../icons/HomeIcon';
 
 const UserMenu = () => {
+  const history = useHistory();
+  const logOut = () => {
+    history.push('/');
+  };
   const onlyWidth = useWindowWidth();
   return (
     <UserMenuStyled>
       <nav className="userMenu-nav">
         <ul className="userMenu-navlist list">
           {mainRoutes.map(
-            ({ isPrivate, path, icon }) =>
+            ({ isPrivate, path, icon, exact }) =>
               isPrivate && (
-                <NavLink to={path} className="userMenu-navitem">
-                  {icon}
-                </NavLink>
+                <li key={path} className="userMenu-navitem">
+                  <NavLink to={path} exact={exact} className="userMenu-navlink">
+                    {icon}
+                  </NavLink>
+                </li>
               ),
           )}
         </ul>
       </nav>
       {onlyWidth < 768 && <UserName />}
+      <button className="logout-btn" type="button" onClick={logOut}>
+        Вихід
+      </button>
     </UserMenuStyled>
   );
 };
