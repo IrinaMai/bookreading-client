1b6f46d651705f1e3a050be5a49ff5da3a2bb618
Viktor Gorokhov <patronusv@gmail.com>
2021-04-22 14:42:17 +0300

    adds books to book reducer on login

--- src/redux/reducers/bookReducer.js
+++ src/redux/reducers/bookReducer.js
@@ -1,6 +1,6 @@
-import { createReducer } from "@reduxjs/toolkit";
-import { postBookSuccess } from "../actions/bookAction";
-
+import { createReducer } from '@reduxjs/toolkit';
+import { postBookSuccess } from '../actions/bookAction';
+import authActions from '../actions/authActions';
 
 const initialState = [
   // {title: '',
@@ -8,15 +8,13 @@ const initialState = [
   // year: "",
   // pages: "",
   // _id: ""}
-]
+];
 
-const bookReducer = createReducer(
-     [...initialState],
-    {
-      [postBookSuccess]  : (state, action) => {
-          return [...state,{...action.payload.data} ]
-      }
-    }
-)
+const bookReducer = createReducer([...initialState], {
+  [postBookSuccess]: (state, action) => {
+    return [...state, { ...action.payload.data }];
+  },
+  [authActions.loginSuccess]: (_, { payload }) => payload.user.books,
+});
 
 export default bookReducer;
