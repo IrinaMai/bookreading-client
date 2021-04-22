594a26628ca323599c1a04ed910f0b1a14b58831
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-04-22 19:36:48 +0300

    add

--- src/pages/libraryPage/LibraryPage.js
+++ src/pages/libraryPage/LibraryPage.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import Modal from '../../components/modal/Modal'
-import getModalState from '../../redux/selectors/modalSelector'
 import modalActions from '../../redux/actions/modalActions'
 
 const LibraryPage = () => {
   const dispatch = useDispatch()
-  const isModal = useSelector(getModalState)
-  const openModal = useSelector(getModalState)
+
+  const handleClick = () => {
+    dispatch(modalActions.toggleModal())
+  }
 
   return (
     <div>
       <h2>Library Page</h2>
-      <button onClick={openModal}>openModal</button>
+      <button onClick={handleClick}>openModal</button>
+      <button onClick={handleClick}>Резюме</button>
       <Modal />
     </div>
   )
