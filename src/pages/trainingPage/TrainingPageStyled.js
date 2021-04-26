bae8023018f78b2e7647019a8a7fef0c49d2dc2f
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2021-04-26 23:42:37 +0300

    added positioning of top page section

--- src/pages/trainingPage/TrainingPageStyled.js
+++ src/pages/trainingPage/TrainingPageStyled.js
@@ -3,5 +3,12 @@ import styled from 'styled-components'
 const TrainingPageWrapper = styled.div`
   padding-top: 25px;
   padding-bottom: 25px;
+
+  .topSection {
+    display:flex;
+    justify-content:space-between;
+  }
+
+
 `
 export default TrainingPageWrapper
--- src/redux/selectors/trainingSelectors.js
+++ src/redux/selectors/trainingSelectors.js
@@ -3,6 +3,7 @@ const getAllBooks = state => state.book
 const getBooksList = state => state.training.beforeStart.booksList
 const getStartDate = state => state.training.beforeStart.startDate
 const getFinishDate = state => state.training.beforeStart.finishDate
+const getActiveTrainingID = state => state.training.active._id
 
 
-export { getAllBooks, getBooksList, getStartDate, getFinishDate };
+export { getAllBooks, getBooksList, getStartDate, getFinishDate, getActiveTrainingID };
