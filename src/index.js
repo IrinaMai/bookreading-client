c3378e8f1db22f704d195aa046a70ea50e701224
DmytroLukianenko <71637913+DmytroLukianenko@users.noreply.github.com>
2021-04-27 09:51:48 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
-import { Provider } from 'react-redux';
-import './assets/styles/fonts.css';
-import '../node_modules/modern-normalize/modern-normalize.css';
-import './assets/styles/index.css';
-import App from './components/App';
-import store, { persistor } from './redux/store';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
+import { Provider } from 'react-redux'
+import './assets/styles/fonts.css'
+import '../node_modules/modern-normalize/modern-normalize.css'
+import './assets/styles/index.css'
+import App from './components/App'
+import store, { persistor } from './redux/store'
+import Timer from './components/timer/Timer'
 
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
+        <Timer />
         <App />
       </BrowserRouter>
     </PersistGate>
   </Provider>,
-  document.getElementById('root'),
-);
+  document.getElementById('root')
+)
