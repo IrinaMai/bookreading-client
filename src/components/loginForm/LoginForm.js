4a97086eeec9977224923d9d1283458e6781afd1
Valentina-git <dert12345f@ukr.net>
2021-04-28 14:15:05 +0300

    edit login

--- src/components/loginForm/LoginForm.js
+++ src/components/loginForm/LoginForm.js
@@ -73,7 +73,7 @@ const LoginForm = () => {
 								</p>
 
 								<Field
-									className='formInput'
+									className='formInput password'
 									type={visiblePassword ? 'text' : 'password'}
 									name='password'
 									value={values.password}
