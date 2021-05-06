2a28fbc510679a6154c56ec5850fa03f1bcadefc
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-05-06 10:22:22 +0300

    add resume

--- src/components/booksList/listItems/beenRead/SummaryBtn.js
+++ src/components/booksList/listItems/beenRead/SummaryBtn.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import Modal from '../../../modal/Modal'
+import Resume from '../../../resume/Resume'
+import modalActions from '../../../../redux/actions/modalActions'
+import { getModalContent } from '../../../../redux/selectors/modalSelector'
+
 import BeenReadStyles from './BeenReadStyles'
 
 const SummaryBtn = () => {
+  const dispatch = useDispatch()
+  const showModal = useSelector(getModalContent)
+  const handleClick = () => {
+    dispatch(modalActions.setModalContent('resume'))
+    dispatch(modalActions.toggleModal())
+  }
   return (
     <BeenReadStyles>
-      <button className='summaryBtn'>Резюме</button>
+      <button type="button" className="summaryBtn" onClick={handleClick}>
+        Резюме
+      </button>
+      {showModal === 'resume' && (
+        <Modal>
+          <Resume />
+        </Modal>
+      )}
     </BeenReadStyles>
   )
 }
